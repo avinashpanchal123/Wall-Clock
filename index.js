8c3e3a858fb88b31aafc1c750162072dd725baab
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2022-03-05 15:54:12 +0530

    minute hour-hand logic done

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
-let hour_hand = document.getElementById("minute_hand");
+let hour_hand = document.getElementById("hour_hand");
+let minute_hand = document.getElementById("minute_hand");
 
-const d = new Date();
-let hour = d.getHours();
-let minutes = d.getMinutes();
+
+  const d = new Date();
+  var hour = d.getHours();
+  var minutes = d.getMinutes();
+
 
 console.log(minutes);
 console.log(hour % 12);
-// initial position of minute hand
-let initialPosition =((minutes) * 6 ) % 360  + 90  ;
 
-hour_hand.style.transform =  `rotate(${initialPosition}deg)`
+// minute hand logic
+let initialPosition = ((minutes * 6) % 360) ;
 
-
-
+minute_hand.style.transform = `rotate(${initialPosition}deg)`;
 
 let interval = setInterval(() => {
-     
-    
-     initialPosition = (initialPosition + 6) % 360
-     console.log(initialPosition);
-     hour_hand.style.transform = `rotate(${initialPosition}deg)`;
-}, 1000 * 60 );
+  initialPosition = (initialPosition + 6) % 360;
+  console.log(initialPosition + " " + "initialPosition");
+  minute_hand.style.transform = `rotate(${initialPosition}deg)`;
+}, 1000 * 60);
+
+//hour hand logic
+let initialHOUR = (((hour+ (minutes/60)) * 360)/ 12) % 360;
+
+console.log(initialHOUR + "initialHOUR");
+
+let hourHand_position = initialHOUR ;
+
+console.log(hourHand_position + "hourhandPosition");
+
+hour_hand.style.transform = `rotate(${hourHand_position}deg)`;
+
+let interval1 = setInterval(() => {
+  hourHand_position = (hourHand_position + 0.5) % 360;
+  console.log(hourHand_position + "  " + "hourHand_position");
+  minute_hand.style.transform = `rotate(${hourHand_position}deg)`;
+}, 1000 * 60);
 
 
-let initialHOUR
+setTimeout(function(){
+    window.location.reload(1);
+    console.log(minutes);
+ }, 10000);
